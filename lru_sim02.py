c1f07640d0ae94b3ac33ea39209e8edadb777b2d
heemingss <163111589+heemingss@users.noreply.github.com>
2024-03-26 17:54:29 +0900

    Update lru_sim02.py

--- lru_sim02.py
+++ lru_sim02.py
@@ -1,40 +1,79 @@
-from list.circularLinkedList import CircularLinkedList
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+        self.tail = new_node
+        self.tail.next = self.head  
+
+    def remove(self, data):
+        current = self.head
+        prev = None
+        while current:
+            if current.data == data:
+                if prev:
+                    prev.next = current.next
+                    if current == self.head:
+                        self.head = current.next
+                    if current == self.tail:
+                        self.tail = prev
+                else:
+                    self.head = current.next
+                    if current == self.tail:
+                        self.tail = None
+                return True
+            prev = current
+            current = current.next
+            if current == self.head:  # Back to the start
+                break
+        return False
+
+    def __contains__(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                return True
+            current = current.next
+            if current == self.head:  # Back to the start
+                break
+        return False
 
 class CacheSimulator:
     def __init__(self, cache_slots):
         self.cache_slots = cache_slots
+        self.cache = CircularLinkedList()
+        self.cache_size = 0
         self.cache_hit = 0
         self.tot_cnt = 0
-        self.cache = CircularLinkedList()
 
     def do_sim(self, page):
         self.tot_cnt += 1
-        # 페이지가 캐시에 이미 존재하는지 확인
-        page_in_cache = False
-        curr = self.cache._CircularLinkedList__tail.next
-        for _ in range(self.cache.size()):
-            if curr.item == page:
-                page_in_cache = True
-                break
-            curr = curr.next
-
-        if page_in_cache:
-            # 페이지가 이미 캐시에 있으면 해당 페이지를 제거하고 다시 추가하여 최근 사용된 것으로 표시
+        if page in self.cache:
             self.cache.remove(page)
             self.cache.append(page)
             self.cache_hit += 1
         else:
-            # 페이지가 캐시에 없으면 새 페이지를 추가
-            if self.cache.size() == self.cache_slots:
-                # 캐시가 가득 찼으면 가장 오래된 페이지를 제거
-                self.cache.pop(0)
+            if self.cache_size >= self.cache_slots:
+                self.cache.remove(self.cache.head.data)
+                self.cache_size -= 1
             self.cache.append(page)
-        
+            self.cache_size += 1
+
     def print_stats(self):
-        print("cache_slot = ", self.cache_slots, "cache_hit = ", self.cache_hit, "hit ratio = ", self.cache_hit / self.tot_cnt)
+        print("cache_slot =", self.cache_slots, "cache_hit =", self.cache_hit, "hit ratio =", self.cache_hit / self.tot_cnt)
 
 if __name__ == "__main__":
-
     data_file = open("./linkbench.trc")
     lines = data_file.readlines()
     for cache_slots in range(100, 1001, 100):
@@ -42,5 +81,4 @@ if __name__ == "__main__":
         for line in lines:
             page = line.split()[0]
             cache_sim.do_sim(page)
-        
         cache_sim.print_stats()
